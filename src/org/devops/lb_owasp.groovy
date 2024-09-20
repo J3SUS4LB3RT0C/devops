a1605dd55da79bb3f122856ba92cc6c8005cdfeb
J3SUS4LB3RT0C <126290556+J3SUS4LB3RT0C@users.noreply.github.com>
2024-09-20 16:39:02 -0500

    Update lb_owasp.groovy

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,11 +1,5 @@
 package org.devops
 
-def AnalisisOwasp(projectGitName){
-sh """ docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
-      --user root --network=${env.NameNetwork} \
-      -t owasp/zap2docker-stable \
-      zap-full-scan.py \
-      -t ${env.dominio} \
-      -r ProjectOwasp.html -I
-      """
+def analisisOWASP(){
+    sh "docker run --rm -v fullscan_react-test_OWASP:/zap/wrk/:rw --user root --network=${env.NameNetwork} -t edansama96/zap2docker-stable zap-full-scan.py -t http://${env.dominio}:5174/ -r fullscan_react-test_OWASP.html -I"
 }
