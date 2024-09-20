54ef8188fe2f5f0f3099b3ac983286b2ec890282
J3SUS4LB3RT0C <126290556+J3SUS4LB3RT0C@users.noreply.github.com>
2024-09-20 16:36:26 -0500

    Update lb_buildimagen.groovy

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
-def buildImageDocker(projectGitName){
-    sh "docker build -t jesuscastellanos/${projectGitName} ."
+def buildImageDocker(projectName){
+    sh "docker build -t ${projectName} ."
 }
