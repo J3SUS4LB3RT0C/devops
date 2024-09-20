9db5dabf1f9d6342a0fe0a29a9d5ce09540c4696
J3SUS4LB3RT0C <126290556+J3SUS4LB3RT0C@users.noreply.github.com>
2024-09-20 15:04:25 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,8 +1,11 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    sh "docker stop ${projectGitName}"
-    sh "docker rm ${projectGitName}"
-    sh "docker pull jesuscastellanos/react-test"
-    sh "docker run -d --name ${projectGitName} --network=${env.NET_NAME} -p 5174:5174 --user root jesuscastellanos/${projectGitName}"
+    //sh "docker stop ${projectGitName}"
+    //sh "docker rm ${projectGitName}"
+    sh "docker pull jesuscastellanos/react-test-jenkinsfile"
+    sh """ docker run -d --name ${projectGitName} \
+    --network=${env.NameNetwork} -p 5174:5174 \
+    --user root jesuscastellanos/${projectGitName}
+    """
 }
